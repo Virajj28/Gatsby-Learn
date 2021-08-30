e3344bb175c4a642472bce646c974a83048136e2
Viraj Jadhav <viraj28j@gmail.com>
2021-08-30 18:19:09 +0530

    Linking pages good to go first

---
+++ learn-gatsby/src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => (
+  <Layout>
+        <div>
+            <h1>About</h1>
+            <p>
+            Nothing to see here just testing.
+            </p>    
+        </div>
+  </Layout>
+    )
+
+export default AboutPage;
---
+++ learn-gatsby/src/pages/services.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const ServicesPage = () =>(
+    <Layout>
+        <div>
+            <h1>ServicesPage</h1>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatum, quisquam.
+            </p>
+        </div>
+    </Layout>
+    )
+
+export default ServicesPage;
